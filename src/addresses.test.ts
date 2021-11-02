b58f9f1c831921fb2eba9f0cfea808fcf1fb39f7
aggre <hiroyuki.aggre@gmail.com>
2021-11-03 00:11:46 +0900

    returns undefined

--- src/addresses.test.ts
+++ src/addresses.test.ts
@@ -6,7 +6,11 @@ test('Returns mainnet address', async (t) => {
 	t.is(res, '0x1510EA12a30E5c40b406660871b335feA32f29A')
 })
 
-test('Returns ropsten address', async (t) => {
-	const res = await addresses({ network: 'ropsten' })
-	t.is(res, '0x609Fe85Dbb9487d55B5eF50451e20ba2Edc8F4B7')
+test('Returns undefined when the passed network is not mainnet', async (t) => {
+	const res1 = await addresses({ network: 'ropsten' })
+	const res2 = await addresses({ network: 'arbitrum-one' })
+	const res3 = await addresses({ network: 'arbitrum-rinkeby' })
+	t.is(res1, undefined)
+	t.is(res2, undefined)
+	t.is(res3, undefined)
 })
--- src/addresses.ts
+++ src/addresses.ts
@@ -3,4 +3,4 @@ import { FunctionAddresses } from '@devprotocol/khaos-core'
 export const addresses: FunctionAddresses = async ({ network }) =>
 	network === 'mainnet'
 		? '0x1510EA12a30E5c40b406660871b335feA32f29A'
-		: '0x609Fe85Dbb9487d55B5eF50451e20ba2Edc8F4B7'
+		: undefined
